2c4f44aca612fd0142858ffc8ccb7ba5967a1a1f
Rihards Krišlauks <rihards.krislauks@gmail.com>
2023-04-15 23:12:50 +0300

    Add timing

--- src/bin/hello-temp-buttons.rs
+++ src/bin/hello-temp-buttons.rs
@@ -1,13 +1,9 @@
 #![no_main]
 #![no_std]
 
-use embedded_hal::{blocking::delay::DelayMs, digital::v2::OutputPin};
-use hal::{gpio::{Output, Pin, PushPull, PullUp, Input}, prelude::InputPin, Temp};
-use nrf52840_hal::{
-    self as hal,
-    gpio::{p0::Parts as P0Parts, Level},
-    Timer,
-};
+use cortex_m::prelude::_embedded_hal_timer_CountDown;
+use hal::Temp;
+use nrf52840_hal::{self as hal, gpio::p0::Parts as P0Parts, Timer};
 
 use airlog::{self as _, peripherals::Button}; // global logger + panicking-behavior + memory layout
 
@@ -41,21 +37,24 @@ fn main() -> ! {
     defmt::println!("Hello, world!");
 
     let board = hal::pac::Peripherals::take().unwrap();
-    // let mut timer = Timer::new(board.TIMER0);
     let pins = P0Parts::new(board.P0);
     let mut temp = Temp::new(board.TEMP);
 
     let mut current_unit = TempUnit::Celsius;
     let mut button = Button::new(pins.p0_11.degrade());
     let mut periodic_timer = Timer::periodic(board.TIMER0);
+    let mut millis: u32 = 0;
 
     loop {
-        let temperature: f32 = temp.measure().to_num();
-        let converted_temp = current_unit.convert_from_celsius(temperature);
-        let converted_symbol = current_unit.get_symbol();
-        defmt::info!("{=f32} {}", converted_temp, converted_symbol);
+        periodic_timer.start(1000_u32);
+        if (millis % 1000) == 0 {
+            let temperature: f32 = temp.measure().to_num();
+            let converted_temp = current_unit.convert_from_celsius(temperature);
+            let converted_symbol = current_unit.get_symbol();
+            defmt::info!("{=f32} {}", converted_temp, converted_symbol);
+        }
 
-        if button.check_rising_edge() {
+        if (millis % 5) == 0 && button.check_rising_edge() {
             current_unit = match current_unit {
                 TempUnit::Fahrenheit => TempUnit::Kelvin,
                 TempUnit::Kelvin => TempUnit::Celsius,
@@ -64,6 +63,7 @@ fn main() -> ! {
             defmt::info!("Unit changed to {}", current_unit);
         }
 
-        periodic_timer.delay_ms(1000_u32);
+        nb::block!(periodic_timer.wait()).unwrap();
+        millis = millis.overflowing_add(1).0;
     }
 }
