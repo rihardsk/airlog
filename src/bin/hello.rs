a92e5132a23c3c91ed37f31084dc00d888de600c
Rihards Krišlauks <rihards.krislauks@gmail.com>
2023-04-15 14:54:06 +0300

    Don't keep the LEDs off

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -1,14 +1,11 @@
 #![no_main]
 #![no_std]
 
+use embedded_hal::{blocking::delay::DelayMs, digital::v2::OutputPin};
 use nrf52840_hal::{
     self as hal,
     gpio::{p0::Parts as P0Parts, Level},
     Timer,
-};
-use embedded_hal::{
-    blocking::delay::DelayMs,
-    digital::v2::OutputPin,
 };
 
 use airlog as _; // global logger + panicking-behavior + memory layout
@@ -26,7 +23,7 @@ fn main() -> ! {
     let mut led_3 = pins.p0_15.into_push_pull_output(Level::High);
     let mut led_4 = pins.p0_16.into_push_pull_output(Level::High);
 
-    for led_states in (1u8 .. 16u8).cycle() {
+    for led_states in (1u8..16u8).cycle() {
         let led_1_state = led_states & 0b0001 > 0;
         let led_2_state = led_states & 0b0010 > 0;
         let led_3_state = led_states & 0b0100 > 0;
@@ -34,23 +31,24 @@ fn main() -> ! {
 
         if led_1_state {
             led_1.set_low().unwrap();
+        } else {
+            led_1.set_high().unwrap();
         }
         if led_2_state {
             led_2.set_low().unwrap();
+        } else {
+            led_2.set_high().unwrap();
         }
         if led_3_state {
             led_3.set_low().unwrap();
+        } else {
+            led_3.set_high().unwrap();
         }
         if led_4_state {
             led_4.set_low().unwrap();
+        } else {
+            led_4.set_high().unwrap();
         }
-        timer.delay_ms(1000u32);
-
-        led_1.set_high().unwrap();
-        led_2.set_high().unwrap();
-        led_3.set_high().unwrap();
-        led_4.set_high().unwrap();
-
         timer.delay_ms(1000u32);
     }
 
