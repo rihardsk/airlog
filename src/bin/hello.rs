f143f6827075bc9e2fb84a192ec33e56c3702bbf
Rihards Krišlauks <rihards.krislauks@gmail.com>
2023-04-15 14:46:33 +0300

    Some more advanced flashing

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -20,12 +20,39 @@ fn main() -> ! {
     let board = hal::pac::Peripherals::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
     let pins = P0Parts::new(board.P0);
-    let mut led_1 = pins.p0_13.into_push_pull_output(Level::Low);
+    // LEDs are active low
+    let mut led_1 = pins.p0_13.into_push_pull_output(Level::High);
+    let mut led_2 = pins.p0_14.into_push_pull_output(Level::High);
+    let mut led_3 = pins.p0_15.into_push_pull_output(Level::High);
+    let mut led_4 = pins.p0_16.into_push_pull_output(Level::High);
 
-    loop {
-        led_1.set_high().unwrap();
+    for led_states in (1u8 .. 16u8).cycle() {
+        let led_1_state = led_states & 0b0001 > 0;
+        let led_2_state = led_states & 0b0010 > 0;
+        let led_3_state = led_states & 0b0100 > 0;
+        let led_4_state = led_states & 0b1000 > 0;
+
+        if led_1_state {
+            led_1.set_low().unwrap();
+        }
+        if led_2_state {
+            led_2.set_low().unwrap();
+        }
+        if led_3_state {
+            led_3.set_low().unwrap();
+        }
+        if led_4_state {
+            led_4.set_low().unwrap();
+        }
         timer.delay_ms(1000u32);
-        led_1.set_low().unwrap();
+
+        led_1.set_high().unwrap();
+        led_2.set_high().unwrap();
+        led_3.set_high().unwrap();
+        led_4.set_high().unwrap();
+
         timer.delay_ms(1000u32);
     }
+
+    airlog::exit()
 }
