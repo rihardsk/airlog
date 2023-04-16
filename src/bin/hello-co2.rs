d02f8eebecf4c695131f6678e566d1696a0b8aaa
Rihards Krišlauks <rihards.krislauks@gmail.com>
2023-04-16 21:40:40 +0300

    Signal co2 level via a rgb led

--- src/bin/hello-co2.rs
+++ src/bin/hello-co2.rs
@@ -8,7 +8,7 @@ use nrf52840_hal::{self as hal, gpio::p0::Parts as P0Parts, Timer};
 
 use airlog::{
     self as _,
-    peripherals::{scd30, Button, SCD30},
+    peripherals::{scd30, Button, LEDControl, SCD30},
 }; // global logger + panicking-behavior + memory layout
 
 #[cortex_m_rt::entry]
@@ -20,6 +20,11 @@ fn main() -> ! {
     let mut temp = Temp::new(board.TEMP);
 
     let mut periodic_timer = Timer::periodic(board.TIMER0);
+
+    let pin_r = pins.p0_03.degrade();
+    let pin_g = pins.p0_04.degrade();
+    let pin_b = pins.p0_28.degrade();
+    let mut led = LEDControl::new(pin_r, pin_g, pin_b);
 
     let scl = pins.p0_30.into_floating_input().degrade();
     let sda = pins.p0_31.into_floating_input().degrade();
@@ -44,6 +49,14 @@ fn main() -> ! {
             }
         }
         let reading = scd30.read_measurement().unwrap();
+        if reading.co2 < 1000_f32 {
+            led.set_state(false, true, false);
+        } else if reading.co2 < 1600_f32 {
+            led.set_state(false, true, true);
+        } else {
+            led.set_state(true, false, false);
+        }
+
         defmt::info!(
             "
             CO2 {=f32} ppm
