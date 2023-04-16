52a1b635a68e595ea34ff6a3aaffe6ba9332df00
Rihards Krišlauks <rihards.krislauks@gmail.com>
2023-04-16 20:55:57 +0300

    Throw out unsed stuff

--- src/bin/hello-co2.rs
+++ src/bin/hello-co2.rs
@@ -11,31 +11,6 @@ use airlog::{
     peripherals::{scd30, Button, SCD30},
 }; // global logger + panicking-behavior + memory layout
 
-#[derive(defmt::Format)]
-enum TempUnit {
-    Kelvin,
-    Celsius,
-    Fahrenheit,
-}
-
-impl TempUnit {
-    fn convert_from_celsius(&self, temp_in_celsius: f32) -> f32 {
-        match self {
-            TempUnit::Kelvin => temp_in_celsius - 273.15,
-            TempUnit::Celsius => temp_in_celsius,
-            TempUnit::Fahrenheit => temp_in_celsius * 9f32 / 5f32 + 32f32,
-        }
-    }
-
-    fn get_symbol(&self) -> &'static str {
-        match self {
-            TempUnit::Kelvin => "K",
-            TempUnit::Celsius => "°C",
-            TempUnit::Fahrenheit => "°F",
-        }
-    }
-}
-
 #[cortex_m_rt::entry]
 fn main() -> ! {
     defmt::println!("Hello, world!");
@@ -44,10 +19,7 @@ fn main() -> ! {
     let pins = P0Parts::new(board.P0);
     let mut temp = Temp::new(board.TEMP);
 
-    let mut current_unit = TempUnit::Celsius;
-    let mut button = Button::new(pins.p0_11.degrade());
     let mut periodic_timer = Timer::periodic(board.TIMER0);
-    let mut millis: u32 = 0;
 
     let scl = pins.p0_30.into_floating_input().degrade();
     let sda = pins.p0_31.into_floating_input().degrade();
@@ -83,24 +55,5 @@ fn main() -> ! {
             reading.rel_humidity
         );
         periodic_timer.delay_ms(5000_u32);
-
-        // if (millis % 1000) == 0 {
-        //     let temperature: f32 = temp.measure().to_num();
-        //     let converted_temp = current_unit.convert_from_celsius(temperature);
-        //     let converted_symbol = current_unit.get_symbol();
-        //     defmt::info!("{=f32} {}", converted_temp, converted_symbol);
-        // }
-
-        // if (millis % 5) == 0 && button.check_rising_edge() {
-        //     current_unit = match current_unit {
-        //         TempUnit::Fahrenheit => TempUnit::Kelvin,
-        //         TempUnit::Kelvin => TempUnit::Celsius,
-        //         TempUnit::Celsius => TempUnit::Fahrenheit,
-        //     };
-        //     defmt::info!("Unit changed to {}", current_unit);
-        // }
-
-        // nb::block!(periodic_timer.wait()).unwrap();
-        // millis = millis.overflowing_add(1).0;
     }
 }
